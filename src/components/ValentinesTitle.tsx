f87e734dab8a885fbebafd1cc32dd6f6ea413a25
JasonTat <112578835+JasonTat@users.noreply.github.com>
2025-02-11 20:05:08 -0700

    completed :)

---
+++ src/components/ValentinesTitle.tsx
@@ -0,0 +1,13 @@
+
+const ValentinesTitle = () => {
+    return (
+        <div className="">
+            <p className="text-black text-6xl titlequestion"
+            >
+                    Yackeline, will you be my valentine?</p>
+        </div>
+    )
+}
+
+export default ValentinesTitle;
+
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,114 +1,116 @@
+'use client';
 import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
+import React from 'react'
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+import { Geist, Geist_Mono, Oooh_Baby } from "next/font/google";
+import { useState, useEffect } from 'react';
+
+//images
+import Brim from "../../public/myphotos/476593795_923259099923893_5547219324307686282_n.jpg"
+import Omen from "../../public/myphotos/lzytrgtqgbbd1.jpg"
+import Kuromi from "../../public/myphotos/my-melody-and-kuromi-valentines-day-hearts-peppeq-lough.jpg"
+
+//my components
+import ValentinesTitle from "../components/ValentinesTitle";
+import NoButton from "../components/NoButton";
+import YesButton from "../components/YesButton";
+
+
+const ooohBaby = Oooh_Baby({
+  variable: "--font-oooh-baby",
   subsets: ["latin"],
+  weight: "400"
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+
 
 export default function Home() {
-  return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/pages/index.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [answer, setAnswer] = useState('');
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  function handleNoClick () {
+    setAnswer('no');
+  }
+  
+  function handleYesClick(){
+    setAnswer('yes');
+  }
+  
+  function handleResetClick(){
+    setAnswer('');
+  }
+  return (
+    <div className="h-screen flex flex-col items-center justify-center">
+      <div className="">
+        <Image src={Kuromi} alt="Kuromi" width={200} height={200}
+          className="mb-6"
+        />
+      </div>
+
+      <div className="mb-4 relative">
+        <ValentinesTitle />
+      </div>
+
+      <div className="mb-6">
+          <div>
+              <button 
+                  className="text-white text-xl bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 mr-3"
+                  onClick={()=>{
+                    handleYesClick()
+                  }}
+              >
+                  YES BABYYY YESSSSSS
+              </button>
+              
+              <button 
+                className="text-white text-xl bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 ml-3 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+                onClick={()=>{
+                  handleNoClick()
+                }}
+              >
+                  WHAT THE HECK NO!! GRRR!!!
+              </button> 
+
+              <button 
+                className="text-white text-xl bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 ml-3 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+                onClick={()=>{
+                  handleResetClick()
+                }}
+              >
+                  reset
+              </button> 
+          </div>
+      </div>
+
+      <div className="">
+        {
+          answer === 'yes' ? 
+          
+          <div className="flex flex-col w-full items-center justify-center ">
+            <h1 className="text-7xl mb-6"> I LOVE YOUUUUUUUUUUUUUUU </h1>
+            <div className="flex flex-row items-center justify-center">
+
+              <Image src={Brim} alt="Brim" className="w-1/6 mr-8" />
+              <img 
+                  src="https://media.tenor.com/008My4n0eLYAAAAi/tkthao219-bubududu.gif" alt="hug" 
+                  className="m-14"
+              />
+              <Image src={Omen} alt="Omen" className="w-1/6 mr-8" />
+              
+              
+            </div>
+            
+
+
+
+
+          </div>
+          :
+          answer === 'no' ?
+          <img src="https://i.pinimg.com/originals/43/ff/e1/43ffe16a0deb6681d0e4074dfda2dc0b.gif" alt="sad" />
+          :
+          <p className="font-bold text-black plsanswer">pls answer :c </p>
+        }
+      </div>
     </div>
   );
 }
